c8b9ac2f2869539fa22e70924de232ec07215f4b
TIUNCHIK Maksim N <maksim.tiunchik@raiffeisen.ru>
2021-10-31 14:58:17 +0300

    full scripts list

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -1,9 +1,29 @@
+create table films
+(
+    code char(5) not null
+        constraint firstkey
+            primary key,
+    title varchar(40) not null,
+    did integer not null,
+    date_prod date,
+    kind varchar(10),
+    len integer not null
+);
+
+alter table films owner to psql;
+
 CREATE OR REPLACE FUNCTION films_notify_change() RETURNS TRIGGER AS $$
 BEGIN
     PERFORM pg_notify('films_changed',row_to_json(NEW)::text);
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
+
+create trigger films_table_change
+    after insert or update or delete
+    on films
+    for each row
+execute procedure films_notify_change();
 
 CREATE TRIGGER films_table_change
     AFTER INSERT OR UPDATE OR DELETE ON films
